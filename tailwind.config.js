431303fb9fec69b411b74720004e234df3d6cf39
Galuh panca <galuhpanca166@gmail.com>
2024-01-14 10:49:37 +0700

    menambahkan file sisanya

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./homepage.{html,js}", "./gallery.{html,js}"],
+  theme: {
+    fontFamily: {
+      quick: ["Quicksand", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
