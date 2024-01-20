e0c9ab354ad12a29d048cfb73ca2bb1f0aa8a479
Galuh panca <galuhpanca166@gmail.com>
2024-01-20 10:56:46 +0700

    menambahkan fungsi dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./homepage.{html,js}", "./gallery.{html,js}"],
+  darkMode: "class",
   theme: {
     fontFamily: {
       quick: ["Quicksand", "sans-serif"],
